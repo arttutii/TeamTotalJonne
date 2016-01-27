f33f226a6c659ad7d5b43ef379bc2b0780a23108
Arttu Tiihonen <arttu.tiihonen@gmail.com>
2016-01-27 15:33:12 +0200

    adding style.css

--- app/script.js
+++ app/script.js
@@ -1,8 +1,7 @@
 angular.module('myApp', ['myModule'])
+
 .controller('GreetingsController', function () {
-  this.greeting = "Hello, is it me you're looking for?";
-  this.who = "Angular";
+  
 
 
 });
-// juliuksen versio
