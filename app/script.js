593cb9eacc803261dd350385984b7861d6eb8803
Julius Niiniranta <julius@niiniranta.net>
2016-01-28 14:50:00 +0200

    hide header on scroll (doesn't work yet!)

--- app/script.js
+++ app/script.js
@@ -4,22 +4,21 @@ angular.module('theApp', [])
     this.content = "";
     var jotain = this;
     $http({
-          method: 'GET',
-          url: 'http://util.mw.metropolia.fi/ImageRekt/api/v2/files'
-        }).then(function successCallback(response) {
-            console.log(response.data);
+        method: 'GET',
+        url: 'http://util.mw.metropolia.fi/ImageRekt/api/v2/files'
+    }).then(function successCallback(response) {
+        console.log(response.data);
 
-             for(var i = 0; i<response.data.length; i++){
-                jotain.content = jotain.content +
-                "pic " + (i+1) + ": " + response.data[i].title
-                + "\n "
+        for (var i = 0; i < response.data.length; i++) {
+            jotain.content = jotain.content +
+                "pic " + (i + 1) + ": " + response.data[i].title + "\n "
                 //+ angular.element(document.getElementById('jee')).append("<img width='100' height='100' src='http://util.mw.metropolia.fi/uploads/"+  response.data[i].path + "''>");
                 + "\n";
-            }
-        }, function errorCallback(response) {
-            jotain.content = "apuva \n";
-            angular.element(document.getElementById('yea')).append(response.data);
-          });
+        }
+    }, function errorCallback(response) {
+        jotain.content = "apuva \n";
+        angular.element(document.getElementById('yea')).append(response.data);
+    });
 
 
 
@@ -30,10 +29,10 @@ angular.module('theApp', [])
     this.content = "";
     var jotain = this;
     $http({
-          method: 'POST',
-          url: 'http://util.mw.metropolia.fi/ImageRekt/api/v2/upload'
-        }).then(function successCallback(response) {
-            /*
+        method: 'POST',
+        url: 'http://util.mw.metropolia.fi/ImageRekt/api/v2/upload'
+    }).then(function successCallback(response) {
+        /*
         Upload a file
 
         POST api/v2/upload
@@ -53,11 +52,42 @@ angular.module('theApp', [])
 
             */
 
-        }, function errorCallback(response) {
-            jotain.content = "someding bad happen \n" + response;
-        });
+    }, function errorCallback(response) {
+        jotain.content = "someding bad happen \n" + response;
+    });
 
 
 
 });
 
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.navbar').outerHeight();
+console.log(navbarHeight);
+console.debug(navbarHeight);
+
+$(window).scroll(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop;
+
+    // Make sure scroll is more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    if (st > lastScrollTop && st > navbarHeight) {
+        $('.navbar').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll up
+        if (st + $(window).height() < $(document).height()) {
+            $('.navbar').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    lastScrollTop = st;
+}
