b21b8b871d2ac260055cd647434d057095eee635
Julius Niiniranta <julius@niiniranta.net>
2016-02-03 16:31:04 +0200

    resolved conflicts

--- app/script.js
+++ app/script.js
@@ -10,18 +10,28 @@ angular.module('theApp', [])
             url: 'http://util.mw.metropolia.fi/ImageRekt/api/v2/files'
         }).then(function successCallback(response) {
             console.log(response.data);
-            angular.element(document.getElementById('jee')).append("<Strong>Gallery size: " + response.data.length + "<Strong> <br>");
+            angular.element(document.getElementById('contents')).append("<Strong>Gallery size: " + response.data.length + "<Strong> <br>");
             for (var i = 0; i < 10; i++) {
                 imagesCount += 1;
-                angular.element(document.getElementById('jee')).append("<img width='100%' height='100%' src='http://util.mw.metropolia.fi/uploads/" + response.data[i].path + "''>" + "<br>" +
-                    "<p class='imgTitle'>img " + (i + 1) + ": " + response.data[i].title + "</p>");
+
+                if (response.data[i] == null) {
+                    /*Do this check only that console doesn't notify null values :D */
+                    break;
+                } else if (response.data[i].type == 'image') {
+                    angular.element(document.getElementById('contents')).append('<img width="100%" height="100%" src="http://util.mw.metropolia.fi/uploads/' + response.data[i].path + '"> <br>' +
+                        "<p class='imgTitle'>" + (i + 1) + ": " + response.data[i].title + "</p>");
+                } else if (response.data[i].type == 'video') {
+                    angular.element(document.getElementById('contents')).append("<video width='100%' height='100%' controls><br> <source src='http://util.mw.metropolia.fi/uploads/" + response.data[i].path + "' type='" + response.data[i].mimeType + "' > </video><br>" +
+                        "<p class='imgTitle'>" + (i + 1) + ": " + response.data[i].title + "</p>");
+                } else if (response.data[i].type == 'audio') {
+                    angular.element(document.getElementById('contents')).append("<audio controls><br> <source src='http://util.mw.metropolia.fi/uploads/" + response.data[i].path + "' type='" + response.data[i].mimeType + "'' > </audio><br>" +
+                        "<p class='imgTitle'>" + (i + 1) + ": " + response.data[i].title + "</p>");
+                }
             }
 
         }, function errorCallback(response) {
-            jotain.content = "apuva \n";
-            angular.element(document.getElementById('jee')).append(response.data);
+            angular.element(document.getElementById('contents')).append(response.data);
         });
-
 
     }
     $scope.showMore = function () {
@@ -30,24 +40,36 @@ angular.module('theApp', [])
             method: 'GET',
             url: 'http://util.mw.metropolia.fi/ImageRekt/api/v2/files'
         }).then(function successCallback(response) {
-            console.log("10 more images shown");
-
-
             var newValue = imagesCount + 10;
 
             for (var i = imagesCount; i < newValue; i++) {
+
                 imagesCount += 1;
-                angular.element(document.getElementById('jee')).append("<img width='100%' height='100%' src='http://util.mw.metropolia.fi/uploads/" + response.data[i].path + "''>" + "<br>" +
-                    "<p class='imgTitle'>img " + (i + 1) + ": " + response.data[i].title + "</p>");
+                if (response.data[i] == null) {
+                    $('#outofpics').show();
+                    $('#showMore').hide();
+                    break;
+                } else if (response.data[i].type == 'image') {
+                    angular.element(document.getElementById('contents')).append('<img width="100%" height="100%" src="http://util.mw.metropolia.fi/uploads/' + response.data[i].path + '"> <br>' +
+                        "<p class='imgTitle'>" + (i + 1) + ": " + response.data[i].title + "</p>");
+                } else if (response.data[i].type == 'video') {
+                    angular.element(document.getElementById('contents')).append("<video width='100%' height='100%' controls><br> <source src='http://util.mw.metropolia.fi/uploads/" + response.data[i].path + "' type='" + response.data[i].mimeType + "' > </video><br>" +
+                        "<p class='imgTitle'>" + (i + 1) + ": " + response.data[i].title + "</p>");
+                } else if (response.data[i].type == 'audio') {
+                    angular.element(document.getElementById('contents')).append("<audio controls><br> <source src='http://util.mw.metropolia.fi/uploads/" + response.data[i].path + "' type='" + response.data[i].mimeType + "'' > </audio><br>" +
+                        "<p class='imgTitle'>" + (i + 1) + ": " + response.data[i].title + "</p>");
+                }
             }
 
         }, function errorCallback(response) {
-            jotain.content = "apuva \n";
-            angular.element(document.getElementById('jee')).append(response.data);
+            angular.element(document.getElementById('contents')).append(response.data);
+
         });
 
     }
+
 })
+
 
 .directive('scrollDetector', function ($window) {
     /* Scroll detector */
@@ -102,24 +124,34 @@ angular.module('theApp', [])
             }
         });
         request.then(function (response) {
-            $('#upSuccess').show();
+
+            if (response.data)
+                $('#upSuccess').show();
             $('#hamburger').click();
-            console.log("success?: " + response.data);
+            angular.element(document.getElementById('contents')).empty();
+            showImages();
+
+
+            console.log("success?: \n" + response.data);
         }, function (error) {
+            $('#upFailed').show();
             console.log("oh dog: " + error.data);
         });
     };
 
 
+})
+
+
+.controller('loginCtrl', function ($scope, $http) {
+    /* content TBA */
+
+
 });
 
 $(document).ready(function () {
-    /* Hiding some stuff */
-    $('#upSuccess, #upFailed').hide();
-    $('.registration').hide();
-
     /*
-        Button click function to open a pop-up window
+        Click functions
     */
     $("#uploadbtn").click(function () {
         $("#uploadModal").modal();
@@ -135,6 +167,38 @@ $(document).ready(function () {
         $('.registration').hide();
         $('.login').fadeIn();
     });
+
+    /* variable for nightmode button*/
+    var clicked = false;
+
+    $('#modebtn').click(function () {
+        if (clicked == false) {
+            $('#navi').attr("class", "navbar navbar-default navbar-fixed-top ");
+            $("body").css("background-color", "white");
+            $('#contentArea').css("background-color", "#ebebeb");
+            $('#contentArea').css("color", "black");
+            $('#modebtn').css("color", "#888888");
+            $('#contents').css("background-color", "#ebebeb");
+            $('#contentsrow').css("background-color", "#ebebeb");
+            $('#contents').css("color", "black");
+            $('#thePage').css("background-color", "white");
+            clicked = true;
+        } else {
+            $('#navi').attr("class", "navbar navbar-default navbar-inverse navbar-fixed-top ");
+            $('body').css("background-color", "#222222");
+            $('#contentArea').css("background-color", "#383838");
+            $('#contentArea').css("color", "white");
+            $('#modebtn').css("color", "white");
+            $('#contents').css("background-color", "#383838");
+            $('#contentsrow').css("background-color", "#383838");
+            $('#contents').css("color", "white");
+            $('#thePage').css("background-color", "#222222");
+            clicked = false;
+        }
+
+    });
+
     /* Executing show images*/
     showImages();
+
 });
